f3ce4e15fd60b68cb8c6b1fcfaf5489e8f827939
HavokAng3L <angelmorin75@gmail.com>
2025-01-01 16:58:34 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# React w/ Material UI
+
+This repo is a template for building React frontends with MaterialUI and TypeScript.
+
+## How to use
+
+- clone to your device.
+- run `npm install` to install required dependencies.
+- then, run `npm run dev` to start the development server.
+- run `npm build` to bundle files for production.
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+export default function App() {
+  return (
+    <>
+      <CssBaseline>
+        <Container maxWidth={"lg"} sx={{ backgroundColor: "primary.main" }}>
+          <Typography variant="h1" color="textPrimary">
+            Hello World
+          </Typography>
+        </Container>
+      </CssBaseline>
+    </>
+  );
+}
